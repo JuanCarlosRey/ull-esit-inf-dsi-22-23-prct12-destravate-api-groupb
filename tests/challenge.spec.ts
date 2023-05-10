5e95457e3b47c1b61eb8f1117fccad9632f222ac
alu0101327679 <alu0101327679@ull.edu.es>
2023-05-10 23:12:53 +0000

    error solved

--- tests/challenge.spec.ts
+++ tests/challenge.spec.ts
@@ -1,148 +1,110 @@
 import request from 'supertest';
 import { expect } from 'chai';
 import { app } from '../src/app.js';
-import { GroupModel } from '../src/models/group.js';
+import { ChallengeModel } from '../src/models/challenge.js';
 
-const testGroup = {
+const testChallenge = {
     id: 0,
-    name: "Grupo de prueba",
-    members: [0, 1, 2, 3],
-    global_stadistics: {
-        _weekly_distance: 17,
-        _weekly_deviation: 6.21,
-        _monthly_distance: 50,
-        _monthly_deviation: 18.18,
-        _annual_distance: 172,
-        _annual_deviation: 57.19
-    },
-    ranking: [1, 3, 2, 0],
-    favorite_tracks: [1, 2],
-    group_history: [{
-        _id: 0,
-        _date: "2021/04/14"
-    },
-    {
-        _id: 1,
-        _date: "2022/05/18"
-    }]
+    name: "Reto de prueba",
+    tracks: [3],
+    type: "correr",
+    long: 10,
+    users: [0, 1]
 }
 
-const testGroup2 = {
+const testChallenge2 = {
     id: 0,
-    name: "Grupo de prueba",
-    members: [0, 1, 2, 3],
-    global_stadistics: {
-        _weekly_distance: 17,
-        _weekly_deviation: 6.21,
-        _monthly_distance: 50,
-        _monthly_deviation: 18.18,
-        _annual_distance: 172,
-        _annual_deviation: 57.19
-    },
-    ranking: [1],
-    favorite_tracks: [1],
-    group_history: [{
-        _id: 0,
-        _date: "2021/04/14"
-    },
-    {
-        _id: 1,
-        _date: "2022/05/18"
-    }]
+    name: "Reto de prueba",
+    tracks: [4],
+    type: "correr",
+    long: 10,
+    users: [0, 1]
 }
 
 beforeEach(async () => {
-    await GroupModel.deleteMany();
-    await new GroupModel(testGroup).save();
+    await ChallengeModel.deleteMany();
+    await new ChallengeModel(testChallenge).save();
 });
 
-describe('POST /groups', () => {
-    it('Should succesfully create a new group', async () => {
-        const response = await request(app).post('/groups').send({
+describe('POST /challenges', () => {
+    it('Should succesfully create a new challenge', async () => {
+        const response = await request(app).post('/challenges').send({
             id: 1,
-            name: "Ciclistas Canarios",
-            members: [1, 3, 7, 12],
-            global_stadistics: {
-                _weekly_distance: 57,
-                _weekly_deviation: 5.18,
-                _monthly_distance: 182,
-                _monthly_deviation: 12.18,
-                _annual_distance: 1428,
-                _annual_deviation: 91.27
-            },
-            ranking: [12, 3, 1, 7],
-            favorite_tracks: [6, 19],
-            group_history: [{
-                _id: 3,
-                _date: "2023/02/28"
-            },
-            {
-                _id: 7,
-                _date: "2023/03/16"
-            }]
+            name: "Reto del Hierro",
+            tracks: [6, 7, 8],
+            type: "bicicleta",
+            long: 57,
+            users: [1, 3, 4, 7]
         }).expect(201);
 
-        const secondGroup = await GroupModel.findById(response.body._id);
-        expect(secondGroup).not.to.be.null;
-        expect(secondGroup!.name).to.equal("Ciclistas Canarios");
+        expect(response.body).to.include({
+            id: 1,
+            name: "Reto del Hierro",
+            type: "bicicleta",
+            long: 57,
+        });
+
+        const secondChallenge = await ChallengeModel.findById(response.body._id);
+        expect(secondChallenge).not.to.be.null;
+        expect(secondChallenge!.name).to.equal("Reto del Hierro");
     });
 
     it("Should get an error", async () => {
-        const response = await request(app).post('/groups').send(testGroup).expect(500);
+        const response = await request(app).post('/challenges').send(testChallenge).expect(500);
     });
 });
 
-describe('GET /groups', () => {
-    it('Should get a group by its name', async () => {
-        await request(app).get('/groups?name=Grupo de prueba').expect(200);
+describe('GET /challenges', () => {
+    it('Should get a challenge by its name', async () => {
+        await request(app).get('/challenges?name=Reto de prueba').expect(200);
     });
 
-    it('Should get a group by its id', async () => {
-        await request(app).get('/groups/0').expect(200);
+    it('Should get a challenge by its id', async () => {
+        await request(app).get('/challenges/0').expect(200);
     });
 
     it('Should get an error', async () => {
-        await request(app).get('/groups?name="Grupo inexistente"').expect(404);
+        await request(app).get('/challenges?name=Reto inexistente').expect(404);
     });
 });
 
-describe('PATCH /groups', () => {
-    it('Should update a group by its name', async () => {
+describe('PATCH /challenges', () => {
+    it('Should update a challenge by its name', async () => {
         const response = await request(app)
-          .patch(`/groups?name=Grupo de prueba`)
-          .send(testGroup2);
+          .patch(`/challenges?name=Usuario de prueba`)
+          .send(testChallenge2);
         expect(response.status).to.be.equal(200);
       });
 
-    it('Should update a group by its id', async () => {
+    it('Should update a challenge by its id', async () => {
     const response = await request(app)
-        .patch(`/groups/0`)
-        .send(testGroup);
+        .patch(`/challenges/0`)
+        .send(testChallenge);
     expect(response.status).to.be.equal(200);
     });
 
     it('Should get an error 404', async () => {
         const response = await request(app)
-            .patch(`/groups/99`)
-            .send(testGroup);
+            .patch(`/challenges/99`)
+            .send(testChallenge);
         expect(response.status).to.be.equal(404);
     });
 });
 
-describe('DELETE /groups', () => {
-    it('Should find and delete a group by its name', async () => {
-        await request(app).delete('/groups?name=Grupo de prueba').expect(200);
+describe('DELETE /challenges', () => {
+    it('Should find and delete a challenge by its name', async () => {
+        await request(app).delete('/challenges?name=Reto de prueba').expect(200);
     });
 
-    it('Should find and delete a group by its id', async () => {
-        await request(app).delete('/groups/0').expect(200);
+    it('Should find and delete a challenge by its id', async () => {
+        await request(app).delete('/challenges/0').expect(200);
     });
 
     it('Should get an error 404', async () => {
-        await request(app).delete('/groups/99').expect(404);
-    }); 
+        await request(app).delete('/challenges/99').expect(404);
+    });
 
     it('Should get an error 400', async () => {
-        await request(app).delete('/groups?not_name=No es un nombre').expect(400);
-    }); 
+        await request(app).delete('/challenges?not_name=No es un nombre').expect(400);
+    });
 });
